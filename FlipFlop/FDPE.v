d9461896292606d14a6ca976fbdd132d07a294a2
YunXiuRZ <70698588+YunXiuRZ@users.noreply.github.com>
2021-06-28 17:49:47 +0800

    6/28
    完善FDCE和FDPE

--- FlipFlop/FDPE.v
+++ FlipFlop/FDPE.v
@@ -1,15 +1,17 @@
 module FDPE (d, clock, clock_enable, preset, q);
     input d, clock, clock_enable, preset;
-    output q;
+    output reg q;
 
-    wire w1, w2, w3, w4, w5, w6;
+    always @(preset) begin
+        if(preset == 1) begin
+            q = 1;
+        end
+    end
 
-    and a1(w1, clock, clock_enable);
-    nand na1(w2, d, w1);
-    not n1(w3, d);
-    nand na2(w4, w1, w3);
-    nand na3(w5, w2, w6);
-    nand na4(w6, w5, w4);
-    or o1(q, preset, w5);
+    always @(posedge clock) begin
+        if(clock_enable == 1 & preset == 0) begin
+            q = d;
+        end
+    end
     
 endmodule
